bdab5e4054c3fde1cf5a87a577df3d2eb095feaa
tesddev <tesleem.amuda@gmail.com>
2024-04-18 17:21:10 +0100

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by GIGL-MAC on 18/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

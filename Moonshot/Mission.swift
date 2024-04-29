cbaaf73974371e60c240041cded639008e7eb8f2
tesddev <tesleem.amuda@gmail.com>
2024-04-29 16:11:48 +0100

    add mission struct

--- Moonshot/Mission.swift
+++ Moonshot/Mission.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+struct Mission: Codable, Identifiable {
+    
+    struct CrewRole: Codable {
+        let name: String
+        let role: String
+    }
+    
+    let id: Int
+    let launchDate: String?
+    let crew: [CrewRole]
+    let description: String
+}

ae009bfaea26c8e0d1ef78c019d47e6b717f6333
tesddev <tesleem.amuda@gmail.com>
2024-04-29 12:40:25 +0100

    import necessary files

---
+++ Moonshot/Astronauts.swift
@@ -0,0 +1,14 @@
+//
+//  Astronauts.swift
+//  Moonshot
+//
+//  Created by GIGL-MAC on 29/04/2024.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}

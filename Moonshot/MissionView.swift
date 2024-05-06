0b4f25e506a5dfe6e13563881e8e165c79f076dd
tesddev <tesleem.amuda@gmail.com>
2024-05-06 14:38:10 +0100

    add mission view and navigate into it

---
+++ Moonshot/MissionView.swift
@@ -0,0 +1,46 @@
+//
+//  MissionView.swift
+//  Moonshot
+//
+//  Created by Tes on 06/05/2024.
+//
+
+import SwiftUI
+
+struct MissionView: View {
+    let mission: Mission
+
+    var body: some View {
+        ScrollView {
+            VStack {
+                Image(mission.image)
+                    .resizable()
+                    .scaledToFit()
+                    .containerRelativeFrame(.horizontal) { width, axis in
+                        width * 0.6
+                    }
+                    .padding(.top)
+
+                VStack(alignment: .leading) {
+                    Text("Mission Highlights")
+                        .font(.title.bold())
+                        .padding(.bottom, 5)
+
+                    Text(mission.description)
+                }
+                .padding(.horizontal)
+            }
+            .padding(.bottom)
+        }
+        .navigationTitle(mission.displayName)
+        .navigationBarTitleDisplayMode(.inline)
+        .background(.darkBackground)
+    }
+}
+
+#Preview {
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+
+    return MissionView(mission: missions[0])
+        .preferredColorScheme(.dark)
+}

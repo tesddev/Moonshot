12212208e04ae531e5d4124dff2ce33d11417994
tesddev <tesleem.amuda@gmail.com>
2024-05-06 10:59:41 +0100

    format date to better and understandable region specific format

--- Moonshot/Mission.swift
+++ Moonshot/Mission.swift
@@ -17,13 +17,17 @@ struct Mission: Codable, Identifiable {
         "apollo\(id)"
     }
     
+    var formattedLaunchDate: String {
+        launchDate?.formatted(date: .abbreviated, time: .omitted) ?? "N/A"
+    }
+    
     struct CrewRole: Codable {
         let name: String
         let role: String
     }
     
     let id: Int
-    let launchDate: String?
+    let launchDate: Date?
     let crew: [CrewRole]
     let description: String
 }
